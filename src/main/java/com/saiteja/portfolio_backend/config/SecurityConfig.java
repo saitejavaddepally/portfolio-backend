c3ec4e6d9057f985d7d5299650da32d5c8a4a14d
saitejavaddepally <saitejavaddepally11@gmail.com>
2026-02-14 23:26:50 +0530

    Update allowed origin for CORS configuration

--- src/main/java/com/saiteja/portfolio_backend/config/SecurityConfig.java
+++ src/main/java/com/saiteja/portfolio_backend/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://portfolio-professional-sepia-rho.vercel.app"
+                "https://porthire.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
