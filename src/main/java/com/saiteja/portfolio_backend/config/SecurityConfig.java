518f5c0b3f033a942532c596c4c0c390e076d371
saitejavaddepally <saitejavaddepally11@gmail.com>
2026-02-15 17:28:23 +0530

    Add recruiter service and controller for managing professionals' data

--- src/main/java/com/saiteja/portfolio_backend/config/SecurityConfig.java
+++ src/main/java/com/saiteja/portfolio_backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
--- src/main/java/com/saiteja/portfolio_backend/repository/UserRepository.java
+++ src/main/java/com/saiteja/portfolio_backend/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.saiteja.portfolio_backend.repository;
 import com.saiteja.portfolio_backend.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
@@ -11,4 +12,5 @@ public interface UserRepository extends MongoRepository<User, String> {
 
     boolean existsByEmail(String email);
 
+    List<User> findByRole(String role);
 }
