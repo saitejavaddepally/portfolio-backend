a3417a5e3180179fecd0d243ff1d0891ab1ab4cc
saitejavaddepally <saitejavaddepally11@gmail.com>
2026-02-27 17:07:02 +0530

    Add authorization to recruiter search endpoint

--- src/main/java/com/saiteja/portfolio_backend/controller/recruiter/RecruiterSearchController.java
+++ src/main/java/com/saiteja/portfolio_backend/controller/recruiter/RecruiterSearchController.java
@@ -3,6 +3,7 @@ package com.saiteja.portfolio_backend.controller.recruiter;
 import com.saiteja.portfolio_backend.service.recruiter.RecruiterSearchService;
 import lombok.RequiredArgsConstructor;
 import org.bson.Document;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class RecruiterSearchController {
     private final RecruiterSearchService recruiterSearchService;
 
     @GetMapping("/search")
+    @PreAuthorize("hasRole('RECRUITER')")
     public List<Document> search(@RequestParam String query) {
         return recruiterSearchService.searchCandidates(query);
     }
